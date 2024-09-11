a8ae65dfa281cd89f34d2ee31bc0972b6b61dea1
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-09-11 14:22:36 +0900

    summation

--- Week02/Week02.c
+++ Week02/Week02.c
@@ -4,6 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a, b; // variable
+	
+	a = 10;
+	b = 20;
+	
 	printf("Hello, Sookmyung Electronics!\n");
+	printf("%d + %d = %d \n", a, b, a+b);
 	return 0;
 }
