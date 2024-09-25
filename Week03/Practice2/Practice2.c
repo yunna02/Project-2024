a503309adf0984ee81e021da32a7fedfc1fe7606
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-09-25 13:56:15 +0900

    Practice2

---
+++ Week03/Practice2/Practice2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	char c;
+	int i;
+	
+	printf("input a number :");
+	scanf("%c", &c);
+	
+	i = c - '0';
+	printf("The input number is %i\n", i);
+	
+	return 0;
+}
