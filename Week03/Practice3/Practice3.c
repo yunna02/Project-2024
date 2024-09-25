7849edba7ba10aefbea32a759c4d025e592c5438
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-09-25 14:06:16 +0900

    Practice3

---
+++ Week03/Practice3/Practice3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	char c;
+	
+	printf("enter a character: ");
+	scanf("%c", &c);
+	
+	printf("the next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+	
+	return 0;
+}
