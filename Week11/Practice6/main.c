07352daa780b53d6460fb498979156d656012a2d
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-11-20 13:40:59 +0900

    week11

--- Week11/Practice6/main.c
+++ Week11/Practice6/main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+/*int main(void) {
 	FILE*fp=NULL;
 	char c;
 	fp = fopen("C:\\Users\\cyndb\\OneDrive\\Document\\sample.txt", "r");
@@ -16,4 +16,24 @@ int main(void) {
     }
 	
 	fclose(fp);
+} */
+
+int main(void) {
+    FILE *fp = NULL;
+    char str[100];
+    int n = 100;
+
+    fp = fopen("C:\\Users\\cyndb\\OneDrive\\Document\\sample.txt", "r");
+    if (fp == NULL) {
+        printf("File not found or cannot be opened.\n");
+        return 1;
+    }
+
+    while (fgets(str, n, fp) != NULL) {
+        printf("%s", str);
+    }
+
+    fclose(fp);
+
+    return 0;
 }
