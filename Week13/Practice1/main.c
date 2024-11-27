c50e79b26fa38ca3756ea5a2257f010d123e0ebe
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-11-27 13:32:36 +0900

    week13

--- Week13/Practice1/main.c
+++ Week13/Practice1/main.c
@@ -10,6 +10,22 @@ struct student{
 };
 
 int main(void){
+	struct student s = {24, "Eunju", 4.2};
+	
+	printf("ID: %d\n", s.ID);
+	printf("name: %s\n", s.name);
+	printf("grade: %f\n", s.grade);
+	
+}
+
+
+/*struct student{
+	int ID;
+	char name[10];
+	double grade;
+};
+
+int main(void){
 	struct student s = {23, "John", 3.5};
 	
 	s.ID = 24;
@@ -20,4 +36,4 @@ int main(void){
 	printf("name: %s\n", s.name);
 	printf("grade: %f\n", s.grade);
 	
-}
+}*/
